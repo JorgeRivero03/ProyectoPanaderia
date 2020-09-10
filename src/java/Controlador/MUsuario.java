3f4a0c384d648fa48453f82de2abfa317f5e8673
jorge_rivero <jorge.skool@hotmail.com>
2020-09-10 13:27:18 -0500

    se agrego la clase MUsuarios y falta realizar el crud

---
+++ src/java/Controlador/MUsuario.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author Jorge Rivero
+ */
+public class MUsuario {
+    private  int id_usu;
+    private String nom_usu, appat_usu, user_usu, pass_usu;
+    
+    //crear el constructor
+    
+    public MUsuario(){
+        
+    }
+    
+    //metodos getters and setters
+
+    public int getId_usu() {
+        return id_usu;
+    }
+
+    public void setId_usu(int id_usu) {
+        this.id_usu = id_usu;
+    }
+
+    public String getNom_usu() {
+        return nom_usu;
+    }
+
+    public void setNom_usu(String nom_usu) {
+        this.nom_usu = nom_usu;
+    }
+
+    public String getAppat_usu() {
+        return appat_usu;
+    }
+
+    public void setAppat_usu(String appat_usu) {
+        this.appat_usu = appat_usu;
+    }
+
+    public String getUser_usu() {
+        return user_usu;
+    }
+
+    public void setUser_usu(String user_usu) {
+        this.user_usu = user_usu;
+    }
+
+    public String getPass_usu() {
+        return pass_usu;
+    }
+
+    public void setPass_usu(String pass_usu) {
+        this.pass_usu = pass_usu;
+    }
+    
+    
+}
